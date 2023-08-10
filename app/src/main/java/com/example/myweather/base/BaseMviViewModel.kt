a4244e0844c31651021adf64512fae5bb426a36e
iamheesoo <beforeheesoo@gmail.com>
2023-08-10 16:37:14 +0900

    [Add] BaseMviViewModel 추가

---
+++ app/src/main/java/com/example/myweather/base/BaseMviViewModel.kt
@@ -0,0 +1,70 @@
+package com.example.myweather.base
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+
+abstract class BaseMviViewModel<State : UiState, Event : UiEvent, Effect : UiEffect> : ViewModel() {
+
+    private val initialState: State by lazy { createInitialState() }
+    abstract fun createInitialState(): State
+
+    val state: State
+        get() = uiState.value
+
+    private val _uiState: MutableStateFlow<State> = MutableStateFlow(initialState)
+    val uiState = _uiState.asStateFlow()
+
+    private val _event: MutableSharedFlow<Event> = MutableSharedFlow()
+    val event = _event.asSharedFlow()
+
+    private val _effect: Channel<Effect> = Channel()
+    val effect = _effect.receiveAsFlow()
+
+    init {
+        subscribeEvents()
+    }
+
+    /**
+     * viewModel이 생성되면 screen의 event를 받을 준비를 한다
+     */
+    private fun subscribeEvents() {
+        viewModelScope.launch {
+            event.collect {
+                handleEvent(it)
+            }
+        }
+    }
+
+    abstract fun handleEvent(event: Event)
+
+    /**
+     * VM -> Screen 단발성 이벤트 전송
+     */
+    protected fun sendEffect(effect: () -> Effect) {
+        val effectValue = effect()
+        viewModelScope.launch { _effect.send(effectValue) }
+    }
+
+    /**
+     * Screen -> VM 유저의 액션 전송
+     */
+    fun sendEvent(event: Event) {
+        viewModelScope.launch { _event.emit(event) }
+    }
+
+    protected fun setState(reduce: State.() -> State) {
+        val newState = state.reduce()
+        _uiState.value = newState
+    }
+}
+
+interface UiState
+interface UiEvent
+interface UiEffect
