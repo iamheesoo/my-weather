97cecc4c73f9d3967cf140af7e25b0c84d945940
iamheesoo <beforeheesoo@gmail.com>
2023-09-18 23:06:22 +0900

    [Update] 검색화면 위치 정보 백그라운드 이미지 라운드 추가

--- app/src/main/java/com/example/myweather/composable/LocationItem.kt
+++ app/src/main/java/com/example/myweather/composable/LocationItem.kt
@@ -1,14 +1,18 @@
 package com.example.myweather.composable
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.paint
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -39,16 +43,22 @@ fun LocationItem(
         }
     Box(
         modifier = Modifier
-            .padding(vertical = 3.dp)
+            .padding(vertical = 5.dp)
             .fillMaxWidth()
-            .paint(
-                painter = painterResource(background),
-                contentScale = ContentScale.FillWidth
-            )
-            .padding(10.dp)
+            .height(150.dp)
     ) {
+        Image(
+            painter = painterResource(id = background),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .matchParentSize()
+                .clip(shape = RoundedCornerShape(16.dp))
+        )
         Column(
-            modifier = Modifier.align(Alignment.TopStart)
+            modifier = Modifier
+                .align(Alignment.TopStart)
+                .padding(10.dp)
         ) {
             Text(
                 text = if (isCurrentLocation) "나의 위치" else locationEntity.name,
@@ -68,7 +78,9 @@ fun LocationItem(
             )
         }
         Column(
-            modifier = Modifier.align(Alignment.TopEnd),
+            modifier = Modifier
+                .align(Alignment.TopEnd)
+                .padding(10.dp),
             horizontalAlignment = Alignment.End
         ) {
             Text(
